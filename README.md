3de407c46e539aca4c19d11d81d82f78d16e29b7
Noctus <jonnyfaulkner21@gmail.com>
2021-05-30 23:50:16 -0500

    added random generation from arrays

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Password_Generator
+
+## Purpose
+A application to randomly generate passwords.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+
+## Contribution
+JavaScript written by Jonathan Faulkner
--- script.js
+++ script.js
@@ -8,27 +8,51 @@ var generatePassword = function(){
       alert("Must be shorter than 128 characters.");
       generatePassword();
     };
+  var lowerConfirm = window.confirm("Would you like lowercase letters");
   var capitalConfirm = window.confirm("Would you like capital letters?");
   var numberConfirm = window.confirm("Would you like numbers?");
   var specialConfirm = window.confirm("Would you like special characters?");
   
-  var lowercaseLetters = [{a:'a'},{b:'b'},{c:'c'},{d:'d'},{e:'e'},{f:'f'},{g:'g'},{h:'h'},{i:'i'},{j:'j'},{k:'k'},{l:'l'},{m:'m'},{n:'n'},{o:'o'},{p:'p'},{q:'q'},{r:'r'},{s:'s'},{t:'t'},{u:'u'},{v:'v'},{w:'w'},{x:'x'},{y:'y'},{z:'z'}];
-  var uppercaseLetters = [{A:'A'},{B:'B'},{C:'C'},{D:'D'},{E:'E'},{F:'F'},{G:'G'},{H:'H'},{I:'I'},{J:'J'},{K:'K'},{L:'L'},{M:'M'},{N:'N'},{O:'O'},{P:'P'},{Q:'Q'},{R:'R'},{S:'S'},{T:'T'},{U:'U'},{V:'V'},{W:'W'},{X:'X'},{Y:'Y'},{Z:'Z'}];
-  var specialCharacters = [" ","!",'"',"#","$","%","&","'",")","(","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\ ","]","^","_","`","{","|","}","~"];
-  var numbers = [{0:'0'},{1:'1'},{2:'2'},{3:'3'},{4:'4'},{5:'5'},{6:'6'},{7:'7'},{8:'8'},{9:'9'}];
-
-  var characterTypeArr = [
-    {1: lowercaseLetters},
-    {2: uppercaseLetters},
-    {3: specialCharacters},
-    {4: numbers}
-  ];
+  var lowercaseLettersArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var uppercaseLettersArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var specialCharactersArr = [" ","!",'"',"#","$","%","&","'",")","(","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\ ","]","^","_","`","{","|","}","~"];
+  var numbersArr = ['0','1','2','3','4','5','6','7','8','9'];
   
-  if (capitalConfirm === true && numberConfirm === true && specialConfirm === true) {
+  var characterTypeArr = [
+    lowercaseLettersArr,
+    uppercaseLettersArr,
+    specialCharactersArr,
+    numbersArr
+  ];
+
+  var randomPass = []
+
+  if (lowerConfirm === true && capitalConfirm === true && numberConfirm === true && specialConfirm === true) {
     for (i = 0; i < passwordLength; i++) {
-      var randomCharType = Math.floor(Math.random() * characterTypeArr.length)
-      console.log(randomCharType, characterTypeArr[randomCharType])
+      var randomCharType = characterTypeArr[Math.floor(Math.random() * characterTypeArr.length)];
+      var randomCharacter = randomCharType[Math.floor(Math.random() * randomCharType.length)];
+      console.log(randomCharacter)
     }
+  } else if (lowerConfirm === true && capitalConfirm === false && numberConfirm === false && specialConfirm === false) {
+    for (i = 0; i < passwordLength; i++){
+      var randomLowercase = lowercaseLettersArr[Math.floor(Math.random() * lowercaseLettersArr.length)]
+      console.log(randomLowercase)
+    } 
+  } else if (lowerConfirm === false && capitalConfirm === true && numberConfirm === false && specialConfirm === false) {
+    for (i = 0; i < passwordLength; i++){
+      var randomUppercase = uppercaseLettersArr[Math.floor(Math.random() * uppercaseLettersArr.length)]
+      console.log(randomUppercase)
+    }
+  } else if (lowerConfirm === false && capitalConfirm === false && numberConfirm === true && specialConfirm === false) {
+    for (i = 0; i < passwordLength; i++){
+      var randomNumbers = numbersArr[Math.floor(Math.random() * numbersArr.length)]
+      console.log(randomNumbers)
+    } 
+  } else if (lowerConfirm === false && capitalConfirm === false && numberConfirm === false && specialConfirm === true) {
+    for (i = 0; i < passwordLength; i++){
+      var randomSpecial = specialCharactersArr[Math.floor(Math.random() * specialCharactersArr.length)]
+      console.log(randomSpecial)
+    } 
   }
 }
 // Get references to the #generate element
